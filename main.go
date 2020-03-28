33d9672f370bb6679da657c414db85178eecb2cd
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-03-28 15:11:28 -0500

    added basic if block

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -9,6 +10,9 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello "
+	if message == "Hello " {
+		fmt.Println("Sucessfully Hit A go Server Endpoint")
+	}
 }
 func main() {
 	http.HandleFunc("/", sayHello)
