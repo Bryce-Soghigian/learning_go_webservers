0bd66360ec1d5f790b894db393226da4f95e12fd
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-03-29 02:14:44 -0500

    server using mux

--- server/server.go
+++ server/server.go
@@ -1,38 +1,74 @@
 package main
 
 import (
+	// "fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type server struct{}
+// func home(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	switch r.Method {
+// 	case "GET":
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(`{"message":"get called"}`))
+// 	case "POST":
+// 		w.WriteHeader(http.StatusCreated)
+// 		w.Write([]byte(`{"message":"post called"}`))
+// 	case "PUT":
+// 		w.WriteHeader(http.StatusAccepted)
+// 		w.Write([]byte(`{"message":"put yeet"}`))
+// 	case "DELETE":
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(`{"message":"delete yeet"}`))
+// 	case "PATCH":
+// 		w.WriteHeader(http.StatusAccepted)
+// 		w.Write([]byte(`{"message": "patches === testing data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitcks?"}`))
+// 	default:
+// 		w.WriteHeader(http.StatusNotFound)
+// 		w.Write([]byte(`{"message":"not found!"}`))
+// 	}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// }
+func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message":"get called"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message":"post called"}`))
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message":"put yeet"}`))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message":"delete yeet"}`))
-	case "PATCH":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "patches === testing data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitesting data type limitcks?"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message":"not found!"}`))
-	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "get called"}`))
+}
 
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post called"}`))
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(`{"message": "put called"}`))
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "delete called"}`))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message": "not found"}`))
 }
 func main() {
-	s := &server{}
-	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", put).Methods(http.MethodPut)
+	r.HandleFunc("/", delete).Methods(http.MethodDelete)
+	r.HandleFunc("/", notFound)
+	port := ":8080"
+	// fmt.Println("========Server Listening On PORT" + port + "=========")
+	log.Fatal(http.ListenAndServe(port, r))
 }
